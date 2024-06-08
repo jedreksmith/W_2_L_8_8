adfd0c888447dba92ae69e8973f0e87916c7cdd4
Andrzej Kowalski <jedreksmith@poczta.onet.pl>
2024-06-08 13:13:14 +0200

    Add project files.

---
+++ W_2_L_8_8/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace W_2_L_8_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string letter;
+            Console.WriteLine("Give me a word");
+            string word = Console.ReadLine();
+
+            for (int i = 0; i<word.Length; i++) 
+            Console.Write(word[word.Length - i - 1]); 
+        }
+    }
+}
